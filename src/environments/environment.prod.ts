9854e8678c78a278b30da73d54c54a4626cb9d95
Jose Chavez <josmchavz@gmail.com>
2024-02-08 08:55:07 -0800

    trying to connect to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,30 +2,20 @@ export const environment = {
   production: true,
   firebase: {
     apiKey: 'AIzaSyCkOojWwzOZp9ozBun2WBYTadAzxKrK0wo',
-
     authDomain: 'tech4good-support.firebaseapp.com',
-
     projectId: 'tech4good-support',
-
     storageBucket: 'tech4good-support.appspot.com',
-
     messagingSenderId: '821987838989',
-
     appId: '1:821987838989:web:8ab8693d1ea437f3161944',
-
+    measurementId: 'G-6GK8XHT31V',
   }, // REPLACE WITH CONFIG
   firebaseLogs: {
     apiKey: 'AIzaSyCkOojWwzOZp9ozBun2WBYTadAzxKrK0wo',
-
     authDomain: 'tech4good-support.firebaseapp.com',
-
     projectId: 'tech4good-support',
-
     storageBucket: 'tech4good-support.appspot.com',
-
     messagingSenderId: '821987838989',
-
-    appId: '1:821987838989:web:c33d7439666d2cc9161944',
-
+    appId: '1:821987838989:web:8ab8693d1ea437f3161944',
+    measurementId: 'G-6GK8XHT31V',
   }, // REPLACE WITH CONFIG
 };
